c492a1cc1d3875457e764d7689492a0f123e6725
Dean Stirrat <37011725+Deanstirrat@users.noreply.github.com>
2018-03-03 17:15:15 -0500

    Update Start.java
    changed name of file and fixed issues that created in the program

--- Start.java
+++ Start.java
@@ -7,7 +7,7 @@ setion 4 */
 import java.util.*;
 import java.io.*;
 
-public class JavaBallTest{
+public class Start{
   public static void main(String [] args) throws IOException{
     
     //VARIABLES:
